7fca9608736f8c5315286ee6ba5930eae5460d89
YuryShageev <schageev.yur@yandex.ru>
2022-09-20 15:48:01 +0300

    Задания 4 и 5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Task 4
+        System.out.println("Task 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend /= 7;
+        System.out.println(friend);
+
+        //Task 5
+        System.out.println("Task 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog *= 10;
+        System.out.println(frog);
+        frog /= 3.5;
+        System.out.println(frog);
+        frog += 4;
+        System.out.println(frog);
     }
 }
